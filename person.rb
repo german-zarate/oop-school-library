4e0113fbdf4b96efb2ee422a7c6746c57b86a503
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-06 22:10:04 +0600

    Add getter and setters for id, name and age as specified.

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
 class Person
+    attr_reader :id
+    attr_accessor :name, :age
     def initialize ( name = "Unknown", age, parent_permission = true)
         @id: Random.rand(1..1000)
         @name: name
