e27cbb6fb3f432e50f524d892d9450d4fe07dd50
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-06 22:07:04 +0600

    Add constructor with name, age, parent_permission with default value.

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 class Person
-    def initialize ()
+    def initialize ( name = "Unknown", age, parent_permission = true)
         @id: Random.rand(1..1000)
         @name: name
         @age: age
+        @parent_permission: parent_permission
     end
 end
