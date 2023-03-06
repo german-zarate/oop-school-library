9972a209579071a368a9ce661a2f5717eb139020
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-06 22:11:15 +0600

    method of_age return if age is greater or equal to 18.

--- person.rb
+++ person.rb
@@ -1,10 +1,15 @@
 class Person
     attr_reader :id
     attr_accessor :name, :age
+
     def initialize ( name = "Unknown", age, parent_permission = true)
         @id: Random.rand(1..1000)
         @name: name
         @age: age
         @parent_permission: parent_permission
+    end
+
+    def of_age?
+        @age >= 18
     end
 end
