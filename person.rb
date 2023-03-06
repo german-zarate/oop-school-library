e30b8eb8c82529e091891a31e7754cd2473d6fe9
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-06 23:02:12 +0600

    Fix linter in person rb file.

--- person.rb
+++ person.rb
@@ -1,19 +1,19 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def initialize ( name = "Unknown", age, parent_permission = true)
-        @id: Random.rand(1..1000)
-        @name: name
-        @age: age
-        @parent_permission: parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
