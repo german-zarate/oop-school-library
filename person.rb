b5e900306b6e52ace77c1edc0151ea2fe9ca34a5
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-08 00:09:21 +0600

    Implement correct_name  method to  return maximum 10 characters of @nameable.correct_name method output.

--- person.rb
+++ person.rb
@@ -24,6 +24,10 @@ class CapitalizeDecorator < Decorator
 end
 
 class TrimmerDecorator < Decorator
+  def correct_name
+    name = @nameable.correct_name
+    name.length > 10 ? name[0..9] : name
+  end
 end
 
 class Person < Nameable
