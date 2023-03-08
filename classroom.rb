7fbdc4aad0ec95bbd31da74bd9359c2cef14c359
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-09 01:30:36 +0600

    Add has-many relationship between Classroom and Student.

--- classroom.rb
+++ classroom.rb
@@ -6,4 +6,9 @@ class Classroom
     @label = label
     @students = []
   end
+
+  def assign_students(student)
+    @students.push(student) unless students.include?(student)
+    student.classroom = self
+  end
 end
