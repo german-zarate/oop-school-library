53e72602ae9c1f8a6a9c17b4e9be4679aafaac68
german-zarate <germanzaratevalbuena@gmail.com>
2023-03-10 23:24:14 +0600

    Add book_list method in app.rb to console books.

--- app.rb
+++ app.rb
@@ -4,4 +4,14 @@ class App
     @people = []
     @rental = []
   end
+
+  def book_list
+    if @books.empty?
+      puts 'book list is empty, try add a new book!'
+    else
+      @books.each_with_index do |book, index|
+        puts "#{index} Title: #{book.title}, Author: #{book.author}"
+      end
+    end
+  end
 end
